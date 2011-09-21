339df01074a4d6c6c436da501c9918735b028bc7
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-21 21:06:48 +0200

    make a start on actually splitting the data

--- lib/file_split.rb
+++ lib/file_split.rb
@@ -1,7 +1,14 @@
 module FileSplit
-  #given a File instance it will partition the data into n new pieces
+  #given a filepath string it will partition the data into n new pieces
   #the original file is not modified
-  def self.split_file_into_n_pieces(file)
+  def self.split_file_into_n_pieces(filepath, num_pieces)
+    file = File.new filepath
     bytes_in_file = file.stat.size
+    input_file = File.basename(filepath)
+    num_pieces.times do |n|
+      File.open "#{input_file}.o#{n}", 'w' do |piece_file|
+        piece_file.print File.read(filepath, bytes_in_file/3)
+      end
+    end
   end
 end
--- spec/file_split_spec.rb
+++ spec/file_split_spec.rb
@@ -4,19 +4,36 @@ describe FileSplit do
   describe "split_file_into_n_pieces(file, n_pieces)" do
     around(:each) do |example|
       require 'fileutils'
-      mkdir 'tmp'
-      cd 'tmp' do
-        example.run
-        `rm .o*`
+      begin
+        FileUtils.mkdir 'tmp'
+        FileUtils.cd 'tmp' do
+          example.run
+        end
+      ensure
+        FileUtils.rm_rf 'tmp'
       end
-      rmdir tmp
     end
 
-    it "splits a given file into n pieces, putting resulting files in the pwd" do
-      file = File.join(File.dirname(__FILE__), *%w[.. media test.mp3])
-      FileSplit.split_file_into_n_pieces(file, 3)
-      %w(test.o1 test.o2 test.o3).each do |filename|
-        File.should exist(filename)
+    describe "on an mp3" do
+      let(:expected_filenames) { %w(test.mp3.o0 test.mp3.o1 test.mp3.o2) }
+      before(:each) do
+        @filepath = File.join(File.dirname(__FILE__), *%w[.. media test.mp3])
+        @return_value = FileSplit.split_file_into_n_pieces(@filepath, 3)
+      end
+
+      it "splits a given file into n pieces, putting resulting files in the pwd" do
+        expected_filenames.each do |filename|
+          File.should exist(filename), "expected file #{filename} to exist"
+        end
+      end
+
+      specify "rejoining the output files gives the same content as the original file" do
+        require 'digest/sha1'
+        bits = ""
+        expected_filenames.each do |filename|
+          bits << File.read(filename)
+        end
+        Digest::SHA1.hexdigest(bits).should == Digest::SHA1.hexdigest(File.read(@filepath))
       end
     end
   end
