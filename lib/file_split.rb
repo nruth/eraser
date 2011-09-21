9b4f3ef119f62afe7eb41fd5a97fda9a518e49d2
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-21 21:57:14 +0200

    refactoring: extracts create_piece from file split

--- lib/file_split.rb
+++ lib/file_split.rb
@@ -1,17 +1,21 @@
 module FileSplit
+  extend(self)
+
   #given a filepath string it will partition the data into n new pieces
   #the original file is not modified
-  def self.split_file_into_n_pieces(filepath, num_pieces)
+  def split_file_into_n_pieces(filepath, num_pieces)
     input_file = File.new filepath
     bytes_in_file = input_file.stat.size
 
-    num_pieces.times do |n|
-      #open this piece's output file for writing
-      #format is original_filename.o[0..num_pieces-1]
-      input_file_name = File.basename(filepath)
-      File.open "#{input_file_name}.o#{n}", 'w' do |piece_file|
-        piece_file.print input_file.read(bytes_in_file/num_pieces)
-      end
+    num_pieces.times do |n| 
+      create_piece("#{File.basename(filepath)}.o#{n}", input_file, bytes_in_file/num_pieces)
+    end
+  end
+
+  private
+  def create_piece(filename, input_stream, piece_size)
+    File.open filename, 'w' do |piece_file|
+      piece_file.print input_stream.read(piece_size)
     end
   end
 end
