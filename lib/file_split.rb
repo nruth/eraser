992b467bc2a4ac3401b4613b4cb74904f10b10f3
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-21 23:45:53 +0200

    refactors padding logic to pad before division of input

--- lib/file_split.rb
+++ lib/file_split.rb
@@ -8,33 +8,25 @@ module FileSplit
     input_filename = File.basename filepath
     bytes_in_file = input_file.stat.size
 
-    # we know how many pieces we want
-    # so let's find how many bytes we will share between them
-    # such that they have an equal byte-length
-    bytes_per_piece = (bytes_in_file/num_pieces)
-
-    # When the file's byte-length is not integer divisible by n pieces
-    # some bytes are left over
-    # if we pad the missing bytes we can increase each blocks's share by 1
+    padded_input_byte_length = bytes_in_file
+    padding_bytes = 0
     leftover_bytes = bytes_in_file % num_pieces
-    eof_padding_bytes = 0
-    if leftover_bytes > 0
-      bytes_per_piece += 1
-      eof_padding_bytes = bytes_per_piece - leftover_bytes
+    unless leftover_bytes == 0
+      padding_bytes = num_pieces - leftover_bytes
+      padded_input_byte_length = bytes_in_file + padding_bytes
+      record_padding(input_filename, padding_bytes)
     end
 
+    bytes_per_piece = padded_input_byte_length /num_pieces
     piece_filenames = (1..num_pieces).map {|n| "#{input_filename}.o#{n}"}
     piece_filenames.each do |piece_filename|
       create_piece(piece_filename, input_file, bytes_per_piece)
     end
 
-    unless eof_padding_bytes == 0
-      #record the padding so it can be ignored on read/reconstruction
-      File.open("#{input_filename}.end_padding", 'w') {|f| f.print eof_padding_bytes}
-
-      #pad the file TODO: spec then add
-      # File.open(piece_filename.last, 'a') {|f| f.print padding_bytes}
-    end
+    # unless padding_bytes == 0
+    #   #pad the file TODO: spec then add
+    #   # File.open(piece_filename.last, 'a') {|f| f.print padding_bytes}
+    # end
   end
 
   private
@@ -42,5 +34,10 @@ module FileSplit
     File.open filename, 'w' do |piece_file|
       piece_file.print input_stream.read(piece_size)
     end
+  end
+
+  #record the padding so it can be ignored on read/reconstruction
+  def record_padding(input_filename, padding_bytes)
+    File.open("#{input_filename}.end_padding", 'w') {|f| f.print padding_bytes } 
   end
 end
