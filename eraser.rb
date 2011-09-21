c7bb39573ac3aee77af66981ebe1e14885aed970
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-22 01:28:26 +0200

    Completes file split w/ padding prototyping
    * pulls out Pieces and Padding modules
    * adds piece assembler

--- eraser.rb
+++ eraser.rb
@@ -2,4 +2,4 @@
 require 'lib/file_split'
 
 mp3 = File.join(File.dirname(__FILE__), *%w[media test.mp3])
-FileSplit.split_file_into_n_pieces(mp3, 3)
+FileSplit.new(mp3).split_file_into_n_pieces(2)
--- lib/file_split.rb
+++ lib/file_split.rb
@@ -1,3 +1,6 @@
+require 'pieces'
+require 'padding'
+
 class FileSplit
   attr_reader :input_filepath
   def initialize(input_filepath)
@@ -7,32 +10,29 @@ class FileSplit
   #given a filepath string it will partition the data into n new pieces
   #the original file is not modified
   def split_file_into_n_pieces(num_pieces)
-    #pad file byte-length to be evenly divisible
-    eof_padding_bytes = determine_padding_bytes(num_pieces)
-    write_padding_metafile(eof_padding_bytes) if eof_padding_bytes > 0
+    eof_padding_bytes = Padding.determine_padding_bytes(num_pieces, bytes_in_file)
 
     bytes_per_piece = (bytes_in_file + eof_padding_bytes) / num_pieces
-    piece_filenames = (1..num_pieces).map {|n| "#{input_filename}.o#{n}"}
+    piece_filenames = Pieces.piece_names(input_filename, num_pieces)
     piece_filenames.each do |piece_filename|
       create_piece(piece_filename, input_file, bytes_per_piece)
     end
 
-    # unless padding_bytes == 0
-    #   #pad the file TODO: spec then add
-    #   # File.open(piece_filename.last, 'a') {|f| f.print padding_bytes}
-    # end
+    if eof_padding_bytes > 0
+      zero_pad_eof(piece_filenames.last, eof_padding_bytes)
+      Padding.write_padding_metafile(input_filename, eof_padding_bytes) 
+    end
   end
 
   private
+  def zero_pad_eof(filename, eof_padding_bytes)
+    File.open(filename, 'a') {|f| f.print(((1..eof_padding_bytes).map {0}).pack('c*'))}
+  end
+
   def create_piece(filename, input_stream, piece_size)
     File.open filename, 'w' do |piece_file|
       piece_file.print input_stream.read(piece_size)
     end
-  end
-
-  #record the padding so it can be ignored on read/reconstruction
-  def write_padding_metafile(padding_bytes)
-    File.open("#{input_filename}.end_padding", 'w') {|f| f.print padding_bytes } 
   end
 
   def input_filename
@@ -42,13 +42,8 @@ class FileSplit
   def input_file
     @_input_file ||= File.open(input_filepath)
   end
-  
+
   def bytes_in_file
     input_file.stat.size
-  end
-  
-  def determine_padding_bytes(num_pieces)
-    leftover_bytes = bytes_in_file % num_pieces
-    padding_bytes = num_pieces - leftover_bytes
   end
 end
