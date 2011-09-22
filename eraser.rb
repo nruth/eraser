011e76996e57ecdf0fd1e28d572153406f393e9f
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-22 18:29:06 +0200

    revise eraser to split and rejoin the test file

--- eraser.rb
+++ eraser.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 require 'lib/file_split'
+require 'lib/piece_assembler'
+
+PIECES = 4
 
 mp3 = File.join(File.dirname(__FILE__), *%w[media test.mp3])
-FileSplit.new(mp3).split_file_into_n_pieces(2)
+FileSplit.new(mp3).split_file_into_n_pieces(PIECES)
+
+File.open('test.mp3', 'w') do |f|
+  f << PieceAssembler.new('test.mp3').build_from_pieces(PIECES)
+end
