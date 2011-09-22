a2ce0f17d32f455e0c22de0d851d0738de3ded06
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-22 18:51:12 +0200

    fixes & tests for combination_xors_to_wanted_piece?

--- spec/piece_assembler_spec.rb
+++ spec/piece_assembler_spec.rb
@@ -22,7 +22,26 @@ describe PieceAssembler do
       PieceAssembler.decode_pieces([0b0001]).should == [0b0111]
     end
   end
-  
+
+  describe "combination_xors_to_wanted_piece?(pieces, combination_of_pieces, wanted_piece)" do
+    let(:pieces) {[0b1000, 0b0110, 0b0011, 0b0100]}
+
+    specify "given a workable solution it returns true" do
+      combination, wanted_piece = 0b1000, 0b1000
+      PieceAssembler.combination_xors_to_wanted_piece?(pieces, combination, wanted_piece).should be_true, "#{format("%#b", combination)} xor combination of #{pieces.map{|p| format("%#b", p)}.join(',')} should match #{format('%#b', wanted_piece)}"
+
+      combination, wanted_piece = 0b0101, 0b0010
+      PieceAssembler.combination_xors_to_wanted_piece?(pieces, combination, wanted_piece).should be_true, "#{format("%#b", combination)} xor combination of #{pieces.map{|p| format("%#b", p)}.join(',')} should match #{format('%#b', wanted_piece)}"
+    end
+
+    specify "given a false solution it returns false" do
+      wanted_piece = 0b1000
+      combinations = [0b0000, 0b0110].each do |combination|
+        PieceAssembler.combination_xors_to_wanted_piece?(pieces, combination, wanted_piece).should be_false, "#{format("%#b", combination)} xor combination of #{pieces.map{|p| format("%#b", p)}.join(',')} should not match #{format('%#b', wanted_piece)}"
+      end
+    end
+  end
+
   describe "elements_indexed_by_bitmask(array, bitmask)" do
     let(:array) {[:a, :b, :c, :d]}
     it "pulls out single elements" do
@@ -31,14 +50,15 @@ describe PieceAssembler do
       PieceAssembler.elements_indexed_by_bitmask(array, 0b0010).should == [:c]
       PieceAssembler.elements_indexed_by_bitmask(array, 0b0001).should == [:d]
     end
-    
+
     it "pulls out all elements with 1111" do
       PieceAssembler.elements_indexed_by_bitmask(array, 0b1111).should == [:a, :b, :c, :d]
     end
-    
+
     it "pulls out 2 elements with 1100 0110 etc" do
       PieceAssembler.elements_indexed_by_bitmask(array, 0b1001).should == [:a, :d]
       PieceAssembler.elements_indexed_by_bitmask(array, 0b1010).should == [:a, :c]
+      PieceAssembler.elements_indexed_by_bitmask(array, 0b0110).should == [:b, :c]
     end
   end
 end
