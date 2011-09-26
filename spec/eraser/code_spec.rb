92fa74d3715a4a4d16f2237684a2f64e05b7275d
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-26 16:40:31 +0200

    changes code class -> module

--- spec/eraser/code_spec.rb
+++ spec/eraser/code_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), *%w[.. .. eraser])
 
 describe "elements_indexed_by_bitmask(array, bitmask)" do
   specify "basis_vectors" do
-    Eraser::Code.new.basis_vectors.sort.should == 
+    Eraser::Code.basis_vectors.sort.should == 
     [0b1000, 0b0110, 0b0100, 0b0011, 0b0010, 0b1101, 0b0001, 0b1010, 0b1100, 0b0101].sort
   end
 
