1df47ca4544e6a59c0c451913ba18842903dfba6
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-09-27 18:30:06 +0200

    remove leftover rubyinline:  * from pieces  * updated gemfile lock

--- lib/eraser/piece.rb
+++ lib/eraser/piece.rb
@@ -1,5 +1,4 @@
 require 'fileutils'
-require 'inline'
 
 module Eraser
   class Piece
@@ -28,60 +27,6 @@ module Eraser
     def self.bitmask_xor(pieces)
       codes = pieces.map(&:bitmask).reduce(:'^')
     end
-
-    # inline do |builder|
-    #   builder.c_raw <<-C_CODE
-    #   #include <stdio.h>
-    # 
-    #   static int content_xor_to_new_file(VALUE filenames) {
-    #     double result = 0;
-    #     long  i, len;
-    #     VALUE *filenames = RARRAY_PTR(filenames);
-    #     len = RARRAY_LEN(filenames);
-    # 
-    #     filenames = 
-    # 
-    #     for(i=0; i<len; i++) {
-    #       result += NUM2DBL(arr[i]);
-    #     }
-    # 
-    #     return rb_float_new(result/(double)len);
-    #   }
-    # 
-    #   int xor_files(FILE *files[], int number_of_files, char *outputfilename) {
-    #     int i;
-    #     int tmp[number_of_files];
-    #     int is_end_of_file = 0;
-    # 
-    #     // init tmp
-    #     for (i = 0; i < number_of_files; i++) tmp[i] = 0;
-    # 
-    #     FILE* output;  
-    #     output = fopen(outputfilename, "wb");
-    # 
-    #     // read a byte from each file and store them in tmp
-    #     while ((tmp[0] = fgetc(files[0])) != EOF) {
-    #       for (i = 1; i < number_of_files; i++) {
-    #         tmp[i] = fgetc(files[i]);
-    #       }
-    # 
-    #       // xor
-    #       int j; 
-    #       int xor_result = 0;
-    #       for (j = 0; j < number_of_files-1; j++) {
-    #         xor_result = tmp[j] ^ tmp[j+1];
-    #       }
-    # 
-    #       // then write to new file
-    #       fputc(xor_result, output);
-    #     }
-    # 
-    #     fclose(output);
-    # 
-    #     return 0;
-    #   }
-    #   C_CODE
-    # end
 
     def self.content_xor_to_new_file(pieces)
       if pieces.length == 1
